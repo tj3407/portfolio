329373a96eb1f9f6d670578d410fae7a26ce62d3
TJ Sornet <tsornet@cisco.com>
2019-12-14 10:45:00 -0800

    Added loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import MainContentLayout from "./main/MainContentLayout"
 import Header from "../components/header"
 import { useStaticQuery, graphql } from "gatsby"
+import { CircularProgress } from "@material-ui/core"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -17,11 +18,21 @@ const IndexPage = () => {
     }
   `)
 
-  return (
-    <Layout>
+  const[show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 2000);
+  }, [])
+  
+  return(
+    <>
+    {show ? (<Layout>
       <Header siteTitle={data.site.siteMetadata.title} />
       <MainContentLayout />
-    </Layout>
+    </Layout>) : <div style={{marginTop: "250px", textAlign: "center"}}><CircularProgress size={150}/></div>}
+    </>
   )
 }
 
