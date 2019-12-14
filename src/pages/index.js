0ae3c93523513a53153ca8b71dc64c9b647b548b
TJ Sornet <tsornet@cisco.com>
2019-12-14 09:34:40 -0800

    Moved header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,26 @@ import "../components/layout.css"
 
 import Layout from "../components/layout"
 import MainContentLayout from "./main/MainContentLayout"
+import Header from "../components/header"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <MainContentLayout />
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <MainContentLayout />
+    </Layout>
+  )
+}
 
 export default IndexPage
