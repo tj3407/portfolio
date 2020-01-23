407753e1322c8d33f320e5e5b3b12be23d67c3e4
TJ Sornet <tsornet@gmail.com>
2020-01-22 22:12:21 -0800

    Added new project

--- src/pages/main/components/LeftContent.js
+++ src/pages/main/components/LeftContent.js
@@ -6,6 +6,7 @@ import GithubContent from "./GithubContent"
 import project1 from "../../../img/project1.jpg"
 import project2 from "../../../img/project2.jpg"
 import project3 from "../../../img/project3.jpg"
+import project4 from "../../../img/project4.jpg"
 
 const useStyle = makeStyles(theme => ({
   root: {
@@ -51,7 +52,7 @@ const useStyle = makeStyles(theme => ({
     marginTop: 30,
   },
   bold: {
-    fontWeight: "600"
+    fontWeight: "600",
   },
 }))
 
@@ -60,157 +61,213 @@ export default function LeftContent() {
 
   return (
     <>
-    <div className={`${classes.root} left-content`}>
-      <Typography variant="h4" display="block" style={{ marginBottom: "20px" }}>
-        Projects
-      </Typography>
-      <Typography
-        variant="h5"
-        display="block"
-        color="textSecondary"
-        align="center"
-        className={classes.bold}
-      >
-        NFL App
-      </Typography>
-      <Typography
-        paragraph
-        display="block"
-        color="textSecondary"
-        align="center"
-      >
-        React, Gatsby, Material-UI, My Sports Feed API
-      </Typography>
-      <div style={{ textAlign: "center" }} className={classes.hasRibbon}>
-        <img src={project1}></img>
-        <div className={classes.ribbon}>
-          <div className={classes.text}>New</div>
-        </div>
-      </div>
-      <Typography paragraph>
-        React application created using Gatsby and utilizing My Sports Feed api
-        and Material-UI for styling.
-      </Typography>
-      <div className={classes.button}>
-        <Button
-          variant="outlined"
-          color="primary"
-          size="large"
-          startIcon={<ExitToAppIcon />}
-          href="http://whosnapit.com/NFL-Fantasy-Football/"
-          target="_blank"
+      <div className={`${classes.root} left-content`}>
+        <Typography
+          variant="h4"
+          display="block"
+          style={{ marginBottom: "20px" }}
         >
-          Demo
-        </Button>
+          Projects
+        </Typography>
+        <Typography
+          variant="h5"
+          display="block"
+          color="textSecondary"
+          align="center"
+          className={classes.bold}
+        >
+          Chat Room App
+        </Typography>
+        <Typography
+          paragraph
+          display="block"
+          color="textSecondary"
+          align="center"
+        >
+          React, Material-UI
+        </Typography>
+        <div style={{ textAlign: "center" }} className={classes.hasRibbon}>
+          <img src={project4}></img>
+          <div className={classes.ribbon}>
+            <div className={classes.text}>New</div>
+          </div>
+        </div>
+        <Typography paragraph>
+          Pure front-end chat/messaging app created using React and Material UI
+          for styling. Data retrieved from local file and implemented simple
+          auto reply with every new message submission.
+        </Typography>
+        <div className={classes.button}>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            startIcon={<ExitToAppIcon />}
+            href="http://whosnapit.com/chat-room-app"
+            target="_blank"
+          >
+            Demo
+          </Button>
+        </div>
+        <hr />
+
+        <Grid
+          container
+          spacing={3}
+          style={{ paddingTop: "20px", marginBottom: "20px" }}
+        >
+          <Grid item xs={12} lg={4}>
+            <div style={{ textAlign: "center" }}>
+              <img src={project1}></img>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} lg={8}>
+            <Typography
+              variant="h5"
+              display="block"
+              color="textSecondary"
+              align="left"
+              className={classes.bold}
+              style={{ marginBottom: "20px" }}
+            >
+              NFL App - React, Gatsby, Material-UI, My Sports Feed API
+            </Typography>
+            <Typography paragraph>
+              React application created using Gatsby and utilizing My Sports
+              Feed api and Material-UI for styling.
+            </Typography>
+            <div style={{ cursor: "pointer" }}>
+              <Link
+                href="http://whosnapit.com/NFL-Fantasy-Football/"
+                target="_blank"
+                variant="h6"
+                display="inline"
+                style={{ verticalAlign: "middle", color: "#3AAA64" }}
+              >
+                <ExitToAppIcon
+                  style={{ verticalAlign: "middle", color: "#3AAA64" }}
+                />{" "}
+                Try It
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+        <hr />
+
+        <Grid
+          container
+          spacing={3}
+          style={{ paddingTop: "20px", marginBottom: "20px" }}
+        >
+          <Grid item xs={12} lg={4}>
+            <div style={{ textAlign: "center" }}>
+              <img src={project3}></img>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} lg={8}>
+            <Typography
+              variant="h5"
+              display="block"
+              color="textSecondary"
+              align="left"
+              className={classes.bold}
+              style={{ marginBottom: "20px" }}
+            >
+              Simple eCommerce Page with Shopping Cart - vanilla JavaScript,
+              jQuery, Bootstrap
+            </Typography>
+            <Typography paragraph>
+              Simple eCommerce page with shopping cart where product information
+              is loaded from a local json file. An option to choose between
+              localStorage or sessionStorage to store products that have been
+              added to cart to test persistency between browser tabs.
+            </Typography>
+            <div style={{ cursor: "pointer" }}>
+              <Link
+                href="http://whosnapit.com/ws-product-cards/"
+                target="_blank"
+                variant="h6"
+                display="inline"
+                style={{ verticalAlign: "middle", color: "#3AAA64" }}
+              >
+                <ExitToAppIcon
+                  style={{ verticalAlign: "middle", color: "#3AAA64" }}
+                />{" "}
+                Try It
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+        <hr />
+
+        <Grid
+          container
+          spacing={3}
+          style={{ paddingTop: "20px", marginBottom: "20px" }}
+        >
+          <Grid item xs={12} lg={4}>
+            <div style={{ textAlign: "center" }}>
+              <img src={project2}></img>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} lg={8}>
+            <Typography
+              variant="h5"
+              display="block"
+              color="textSecondary"
+              align="left"
+              className={classes.bold}
+              style={{ marginBottom: "20px" }}
+            >
+              MLB Stats App - Angular 5, Bootstrap, My Sports Feed API
+            </Typography>
+            <Typography paragraph>
+              MLB stats application created using Angular 5, My Sports Feed api
+              for data and Bootstrap for styling.
+            </Typography>
+            <div style={{ cursor: "pointer" }}>
+              <Link
+                href="http://tj-mlb-demo.herokuapp.com/"
+                target="_blank"
+                variant="h6"
+                display="inline"
+                style={{ verticalAlign: "middle", color: "#3AAA64" }}
+              >
+                <ExitToAppIcon
+                  style={{ verticalAlign: "middle", color: "#3AAA64" }}
+                />{" "}
+                Try It
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+        <hr />
+
+        <Grid
+          container
+          spacing={3}
+          style={{ paddingTop: "20px", marginBottom: "20px" }}
+        >
+          <Grid item xs={12}>
+            <Typography paragraph align="center" className={classes.bold}>
+              ...LOAD MORE
+            </Typography>
+          </Grid>
+        </Grid>
       </div>
-      <hr />
-
-      <Grid
-        container
-        spacing={3}
-        style={{ paddingTop: "20px", marginBottom: "20px" }}
-      >
-        <Grid item xs={12} lg={4}>
-          <div style={{ textAlign: "center" }}>
-            <img src={project3}></img>
-          </div>
-        </Grid>
-
-        <Grid item xs={12} lg={8}>
-          <Typography
-            variant="h5"
-            display="block"
-            color="textSecondary"
-            align="left"
-            className={classes.bold}
-            style={{ marginBottom: "20px" }}
-          >
-            Simple eCommerce Page with Shopping Cart - vanilla JavaScript, jQuery, Bootstrap
-          </Typography>
-          <Typography paragraph>
-            Simple eCommerce page with shopping cart where product information
-            is loaded from a local json file. An option to choose between
-            localStorage or sessionStorage to store products that have been
-            added to cart to test persistency between browser tabs.
-          </Typography>
-          <div style={{ cursor: "pointer" }}>
-            <Link
-              href="http://whosnapit.com/ws-product-cards/"
-              target="_blank"
-              variant="h6"
-              display="inline"
-              style={{ verticalAlign: "middle", color: "#3AAA64" }}
-            >
-              <ExitToAppIcon
-                style={{ verticalAlign: "middle", color: "#3AAA64" }}
-              />{" "}
-              Try It
-            </Link>
-          </div>
-        </Grid>
-      </Grid>
-      <hr />
-
-      <Grid
-        container
-        spacing={3}
-        style={{ paddingTop: "20px", marginBottom: "20px" }}
-      >
-        <Grid item xs={12} lg={4}>
-          <div style={{ textAlign: "center" }}>
-            <img src={project2}></img>
-          </div>
-        </Grid>
-
-        <Grid item xs={12} lg={8}>
-          <Typography
-            variant="h5"
-            display="block"
-            color="textSecondary"
-            align="left"
-            className={classes.bold}
-            style={{ marginBottom: "20px" }}
-          >
-            MLB Stats App - Angular 5, Bootstrap, My Sports Feed API
-          </Typography>
-          <Typography paragraph>
-            MLB stats application created using Angular 5, My Sports Feed api
-            for data and Bootstrap for styling.
-          </Typography>
-          <div style={{ cursor: "pointer" }}>
-            <Link
-              href="http://tj-mlb-demo.herokuapp.com/"
-              target="_blank"
-              variant="h6"
-              display="inline"
-              style={{ verticalAlign: "middle", color: "#3AAA64" }}
-            >
-              <ExitToAppIcon
-                style={{ verticalAlign: "middle", color: "#3AAA64" }}
-              />{" "}
-              Try It
-            </Link>
-          </div>
-        </Grid>
-      </Grid>
-      <hr />
-
-      <Grid
-        container
-        spacing={3}
-        style={{ paddingTop: "20px", marginBottom: "20px" }}
-      >
-        <Grid item xs={12}>
-          <Typography paragraph align="center" className={classes.bold}>...LOAD MORE</Typography>
-        </Grid>
-      </Grid>
-    </div>
-    <div className={`${classes.root} left-content`}>
-      <Typography variant="h4" display="block" style={{ marginBottom: "20px" }}>
-        My GitHub
-      </Typography>
-      <GithubContent />
-    </div>
+      <div className={`${classes.root} left-content`}>
+        <Typography
+          variant="h4"
+          display="block"
+          style={{ marginBottom: "20px" }}
+        >
+          My GitHub
+        </Typography>
+        <GithubContent />
+      </div>
     </>
   )
 }
