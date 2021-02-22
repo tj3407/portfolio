213afeb21d1d2bf843aa50f662844d4f7b06e9b0
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-02-21 19:14:40 -0800

    Add image metatag

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Portfolio Page`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    image: '/undraw_portfolio_website_lidw.png'
+    image: '/images/undraw_portfolio_website_lidw.png'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
